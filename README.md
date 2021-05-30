62d3aa7f06e6a6b0647025a217f29312fbcc7577
MarcosATSF <marcos_atsf@hotmail.com>
2021-05-30 17:40:10 -0300

    First complete steps of ConEg-Inspector (v1)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# inspetint
-Intelligent inspector which detects faces (not)/wearing masks
+# ConEg-Inspector
+Intelligent inspector which detects faces (not) wearing masks
+
+At the current state of this project, NN model is already created so there's no
+need to train it again (unless you want to, follow [this](train/train_inspector.ipynb)notebook).
+
+![Example](assets/example.png "Running example")
+
+## How-to Run
+Given to some OpenCV limitations, it's recommended to run this on a Windows machine
+with Python 3.7 (tested with 3.7.9) and install libs from [requirements](requirements.txt).
+First check if [configuration](config/config_insp.yaml) file is according the expected by your needs.
+With all set-up, run `python detect_mask_video.py` to start the live face detection.
+
+## Caveat
+As this project is directly related to [ConEg repo](https://github.com/marcosatsf/coneg-project), it's recommended run the whole project in order to see the expected result when this was built, this means, when detected a face with or no mask, a request is sent to an API project that will handle that event.
---
+++ send_policy.py
@@ -0,0 +1,92 @@
+from threading import Lock, Thread
+from time import sleep, time
+import requests
+import yaml
+import os
+
+class SendPolicy:
+    def __init__(self) -> None:
+        """
+        Initialize SendPolicy module, establishing some vars.
+        """
+        self.ready = True
+        self.mutex = Lock()
+        self.URL = 'http://localhost:8000/found'
+
+        with open(f'config{os.sep}config_insp.yaml', 'r') as f:
+            self.vars = yaml.load(f)
+
+        self.time = self.vars['delay']
+        self.request_info = {
+            'ts': self.get_now(),
+            'location': self.vars['location']
+        }
+        self.upload_info = {
+            'file_uploaded': None
+        }
+
+
+    def get_now(self) -> int:
+        """
+        Returns current floor time.
+
+        Returns:
+            int: timestamp current time
+        """
+        return int(time())
+
+
+    def send(self, frame_in_bytes: bytes) -> None:
+        """
+        Start processing of send packet to API, verifying if is possible
+        given the delay value.
+
+        Args:
+            frame_in_bytes (bytes): bytes to send as a file
+        """
+        if self.ready:
+            self.ready = False
+            exe = Thread(target=self.send2api, args=(frame_in_bytes,))
+            exe.start()
+
+
+    def send2api(self, frame_in_bytes: bytes) -> None:
+        """
+        Sends data to API. If not wearing mask, sends frame also.
+
+        Args:
+            frame_in_bytes (bytes): bytes to send as a file
+        """
+        # get current time
+        self.request_info['ts'] = self.get_now()
+
+        # build file structure
+        if frame_in_bytes:
+            self.upload_info['file_uploaded'] = (
+                    'gotcha_frame.jpg',
+                    frame_in_bytes
+                )
+        else:
+            self.upload_info['file_uploaded'] = None
+
+        # send req. / receive response to API
+        response = requests.post(
+                self.URL,
+                data=self.request_info,
+                files=self.upload_info
+            )
+        # print status
+        print(response.status_code, response.json(), sep=' -|- ')
+        # give a break of request! hehe
+        self.wait_until_ready(self.time)
+
+
+    def wait_until_ready(self, time2wait):
+        """
+        Sleeps for <time2wait> seconds
+
+        Args:
+            time2wait (int): Seconds to wait
+        """
+        sleep(time2wait)
+        self.ready = True
